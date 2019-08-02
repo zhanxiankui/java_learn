019975d1c69e61c9cb12ed07c15bd205f1e05ce5
3379517762 <3379517762@qq.com>
2019-08-02 14:10:01 +0800

    提交合并等练习

---
+++ src/com/commons/studyone/FileToByte.java
@@ -0,0 +1,75 @@
+package com.studyone;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+
+import org.junit.Test;
+
+/**
+ *   文件内容转成字节数组，文件以字节流处理。
+ * <p>Copyright: Copyright (c) 2019</p>
+ * <p>succez</p>
+ * @author zhanxk
+ * @createdate 2019年8月1日
+ */
+
+public class FileToByte {
+
+	/**
+	 * 将文件内容转换成byte数组返回,如果文件不存在或者读入错误返回null
+	 * @throws Exception 
+	 */
+	public byte[] file2buf(File fobj) throws Exception {
+		FileInputStream fis;
+		ByteArrayOutputStream bos;
+		byte[] end;
+
+		if (fobj != null && fobj.exists()) {
+
+			fis = new FileInputStream(fobj);
+			try {
+				bos = new ByteArrayOutputStream(1024);
+				try {
+
+					byte[] bt = new byte[1024];
+					int len = -1;
+					while ((len = fis.read(bt)) != -1) {
+						bos.write(bt, 0, len);
+
+					}
+
+					end = bos.toByteArray();
+
+				}
+				finally {
+					bos.close();
+				}
+
+			}
+			finally {
+
+				fis.close();
+
+			}
+
+			return end;
+
+		}
+
+		return null;
+	}
+
+	@Test
+	public void test() throws Exception {
+		
+		String pathname=this.getClass().getResource("").getFile();  //获取java文件所在的目录。
+		File f=new File(pathname+"test.txt");
+		byte[] bs = file2buf(f);
+
+		for (byte b : bs) {
+			System.out.println("----" + b);
+		}
+	}
+
+}
---
+++ src/com/commons/studyone/IntToHex.java
@@ -0,0 +1,82 @@
+package com.studyone;
+import org.junit.Test;
+
+/**
+ * 整数转成16进制
+ * <p>Copyright: Copyright (c) 2019</p>
+ * <p>succez</p>
+ * @author w
+ * @createdate 2019年8月2日
+ */
+
+public class IntToHex {
+	private final int targ = 16; //需要转换的进制。
+
+	/**
+	 *  整数转成16进制
+	 * @param num
+	 * @return
+	 */
+	public String int2Hex(int num) {
+
+		StringBuilder sb = new StringBuilder();
+
+		int temp = num / targ;
+		int mark = num % targ;
+
+		sb.append(num2Char(mark));
+
+		while (temp > 0) {
+			num = temp;
+			mark = num % targ;
+			sb.append(num2Char(mark));
+			temp = num / targ;
+
+		}
+
+		return sb.reverse().toString();
+	}
+
+	/** 数字变成对应的字符串
+	* 10进制以上的数在计算机中以 A,B,C,D等表示。这个方法只对应了16进制以下。
+	* @param n
+	* @return
+	*/
+
+	public String num2Char(int n) {
+		if (n < 10) {
+			return String.valueOf(n);
+		}
+
+		String ch = "";
+		switch (n) {
+			case 10:
+				ch = "A";
+				break;
+			case 11:
+				ch = "B";
+				break;
+			case 12:
+				ch = "C";
+				break;
+			case 13:
+				ch = "D";
+				break;
+			case 14:
+				ch = "E";
+				break;
+			case 15:
+				ch = "F";
+				break;
+		}
+		return ch;
+
+	}
+
+	@Test
+	public void test() {
+		System.out.println("=====" + int2Hex(64));
+
+	}
+
+}
